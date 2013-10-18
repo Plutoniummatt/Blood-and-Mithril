c7193e95fd58069fd27f385502f8f9c4bd87e6f3
plutomattp <plutomattp@66836f89-9df8-47b4-b0e3-7a98460e9a9e>
2013-10-18 13:40:35 +0000

    Move ToDoList

---
+++ trunk/source/src/spritestar/ReleaseToDoList.java
@@ -0,0 +1,17 @@
+package spritestar;
+/**
+ * To-do list before ALPHA release
+ *
+ * <b><p> Generation                                                                                     </b></p>
+ * <b><p> Terraforming - Mining & Placing blocks                                                         </b></p>
+ * <b><p> Building and Construction (Building construction and decontruction framework)                  </b></p>
+ * <b><p> Resource gathering and stockpiling (At least the following: farming, hunting, mining, foraging)</b></p>
+ * <b><p> Props (Trees, rocks, etc, and wiring these into generation)                                    </b></p>
+ * <b><p> Combat (New animations, equipment framework + equipment animation framework)                   </b></p>
+ * <b><p> Main menu screen                                                                               </b></p>
+ * <b><p> Multiple saved games (Huge refactors of current statics various everywhere)                    </b></p>
+ * <b><p> Text input (Renaming elves, setting save path etc)                                             </b></p>
+ * <b><p> At least 5 types of NPC                                                                        </b></p>
+ * <b><p> Sound                                                                                          </b></p>
+ */
+public class ReleaseToDoList {}
