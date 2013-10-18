bebaad0e291c301fc6d9d70092f6035df93d0838
plutomattp <plutomattp@66836f89-9df8-47b4-b0e3-7a98460e9a9e>
2013-10-18 14:16:48 +0000

    Tidy

--- source/src/spritestar/generation/TerrainGenerator.java
+++ source/src/spritestar/generation/TerrainGenerator.java
@@ -11,23 +11,23 @@ import spritestar.world.topography.tile.Tile;
  */
 public class TerrainGenerator {
 
-	public void generate(int x, int y) {
+	public void generate(int chunkX, int chunkY) {
 
 		// Makes arrays of tiles to work on. both for foreground and background
 		Tile[][] fTiles = new Tile[Topography.chunkSize][Topography.chunkSize];
 		Tile[][] bTiles = new Tile[Topography.chunkSize][Topography.chunkSize];
 
 		// Populate the tile arrays.
-		populateTileArrays(x, y, fTiles, bTiles);
+		populateTileArrays(chunkX, chunkY, fTiles, bTiles);
 
 		// Create the chunk and put it in the ChunkMap.
-		Chunk newChunk = new Chunk(fTiles, bTiles, x, y);
-		Topography.chunkMap.addChunk(x, y, newChunk);
+		Chunk newChunk = new Chunk(fTiles, bTiles, chunkX, chunkY);
+		Topography.chunkMap.addChunk(chunkX, chunkY, newChunk);
 	}
 
 
 	/** Populates tile arrays with tiles, determined by the structures */
-	private void populateTileArrays(int x, int y, Tile[][] fTiles, Tile[][] bTiles) {
+	private void populateTileArrays(int chunkX, int chunkY, Tile[][] fTiles, Tile[][] bTiles) {
 		for (int tileX = 0; tileX < Topography.chunkSize; tileX++) {
 			for (int tileY = 0; tileY < Topography.chunkSize; tileY++) {
 				// TODO populateTileArrays
--- source/src/spritestar/util/datastructure/Box.java
+++ source/src/spritestar/util/datastructure/Box.java
@@ -5,8 +5,8 @@ import java.io.Serializable;
 import com.badlogic.gdx.math.Vector2;
 
 /**
- * A box whose position is defined as the middle of box
- * 
+ * A box whose position is defined as the centre of box
+ *
  * @author Matt
  */
 public class Box implements Serializable {
@@ -14,7 +14,7 @@ public class Box implements Serializable {
 
 	/** Dimensions of the box */
 	public float width, height;
-	
+
 	/** Position of the box */
 	public Vector2 position;
 
@@ -27,13 +27,13 @@ public class Box implements Serializable {
 		this.height = height;
 	}
 
-	
+
 	/** True if a location is within this {@link Box} */
 	public boolean isWithinBox(Vector2 location) {
-		return 
-			location.x > position.x - width / 2 && 
-			location.x < position.x + width / 2 && 
-			location.y > position.y - height / 2 && 
+		return
+			location.x > position.x - width / 2 &&
+			location.x < position.x + width / 2 &&
+			location.y > position.y - height / 2 &&
 			location.y < position.y + height / 2
 		;
 	}
