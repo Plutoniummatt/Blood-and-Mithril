0e5dbc473283896895f3cf2be342c855b5a04f4d
plutomattp <plutomattp@66836f89-9df8-47b4-b0e3-7a98460e9a9e>
2013-10-30 14:28:39 +0000

    Render more debug aids (Chunk borders, current mouse over tile) Changes to celestial orbital radius

--- trunk/source/src/bloodandmithril/world/weather/Weather.java
+++ trunk/source/src/bloodandmithril/world/weather/Weather.java
@@ -6,7 +6,6 @@ import static java.lang.Math.pow;
 import java.util.ArrayList;
 import java.util.List;
 
-
 import bloodandmithril.Fortress;
 import bloodandmithril.util.Shaders;
 import bloodandmithril.world.Epoch;
@@ -35,15 +34,15 @@ public class Weather {
 
 	/** Texture regions for various phases of the moon */
 	private static List<TextureRegion> moonPhases = new ArrayList<TextureRegion>();
-	
+
 	private static TextureRegion sunTexture = new TextureRegion(GameWorld.gameWorldTexture, 0, 1536, 512, 512);
 	private static TextureRegion sunGlowTexture = new TextureRegion(GameWorld.gameWorldTexture, 512, 1536, 512, 512);
 
 	/** Orbital radius of the moon around the {@link #celestialPivot} */
-	private static float celestialOrbitalRadius = 1400f;
+	private static float celestialOrbitalRadius = Fortress.WIDTH;
 
 	/** The pivot around which the moon resolves */
-	private static Vector2 celestialPivot = new Vector2(Fortress.WIDTH/2, -celestialOrbitalRadius/2);
+	private static Vector2 celestialPivot = new Vector2(Fortress.WIDTH/2, -Fortress.HEIGHT/2);
 
 	/** The index to access {@link #moonPhases} with, updated between 11:00 and 13:00 */
 	private static int moonPhaseIndex = (int)(12f / 30f * WorldState.currentEpoch.dayOfMonth);
@@ -67,12 +66,12 @@ public class Weather {
 	/** Renders the sun */
 	private static void renderSun() {
 		float time = WorldState.currentEpoch.getTime();
-		
+
 		double angle = time < 12f ? Math.PI/2f + Math.PI * (time- 2f)/20f : Math.PI + Math.PI * (time - 12f)/20f;
-		
+
 		float x = celestialPivot.x - celestialOrbitalRadius * (float)Math.sin((float)angle) - sunTexture.getRegionWidth()/2;
 		float y = celestialPivot.y - celestialOrbitalRadius * (float)Math.cos((float)angle) - sunTexture.getRegionHeight()/2;
-		
+
 		Fortress.spriteBatch.begin();
 		Shaders.sun.begin();
 		Shaders.sun.setUniformf("time", time);
@@ -111,27 +110,27 @@ public class Weather {
 
 		float time = WorldState.currentEpoch.getTime();
 		Color filter = new Color();
-		
+
 		if (time < 10) {
-			filter.r = (float) (0.1D + 1.2D * exp(-0.100*pow((time-10), 2)));
-			filter.g = (float) (0.1D + 1.2D * exp(-0.150*pow((time-10), 2)));
-			filter.b = (float) (0.1D + 1.2D * exp(-0.200*pow((time-10), 2)));
+			filter.r = (float) (0.1D + 1.2D * exp(-0.100*pow(time-10, 2)));
+			filter.g = (float) (0.1D + 1.2D * exp(-0.150*pow(time-10, 2)));
+			filter.b = (float) (0.1D + 1.2D * exp(-0.200*pow(time-10, 2)));
 		} else if (time >= 10 && time < 14) {
 			filter.r = 1.3f;
 			filter.g = 1.3f;
 			filter.b = 1.3f;
 		} else {
-			filter.r = (float) (0.1D + 1.2D * exp(-0.100*pow((time-14), 2)));
-			filter.g = (float) (0.1D + 1.2D * exp(-0.150*pow((time-14), 2)));
-			filter.b = (float) (0.1D + 1.2D * exp(-0.200*pow((time-14), 2)));
+			filter.r = (float) (0.1D + 1.2D * exp(-0.100*pow(time-14, 2)));
+			filter.g = (float) (0.1D + 1.2D * exp(-0.150*pow(time-14, 2)));
+			filter.b = (float) (0.1D + 1.2D * exp(-0.200*pow(time-14, 2)));
 		}
-		
+
 		Color topColor = dayTopColor.cpy().mul(WorldState.currentEpoch.dayLight()).add(nightTopColor.cpy().mul(1f - WorldState.currentEpoch.dayLight())).mul(filter);
 		Color bottomColor = dayBottomColor.cpy().mul(WorldState.currentEpoch.dayLight()).add(nightBottomColor.cpy().mul(1f - WorldState.currentEpoch.dayLight())).mul(filter);
 
 		shapeRenderer.filledRect(0, 0, Fortress.WIDTH, Fortress.HEIGHT, bottomColor, bottomColor, topColor, topColor);
 		shapeRenderer.end();
-		
+
 		shapeRenderer.begin(ShapeType.FilledCircle);
 		shapeRenderer.end();
 	}
