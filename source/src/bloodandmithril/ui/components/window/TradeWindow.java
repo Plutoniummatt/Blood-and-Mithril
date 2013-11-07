75cc02fb6d6a77da3fb112efcfd8653d5539f5cb
plutomattp <plutomattp@66836f89-9df8-47b4-b0e3-7a98460e9a9e>
2013-11-07 17:27:18 +0000

    Panels, Scrollbars prototyping

--- source/src/bloodandmithril/ui/components/window/TradeWindow.java
+++ source/src/bloodandmithril/ui/components/window/TradeWindow.java
@@ -6,6 +6,10 @@ import java.util.List;
 import bloodandmithril.item.Container;
 import bloodandmithril.ui.components.Component;
 import bloodandmithril.ui.components.ContextMenu;
+import bloodandmithril.ui.components.Panel;
+import bloodandmithril.ui.components.panel.ScrollableListingPanel;
+
+import com.badlogic.gdx.graphics.Color;
 
 /**
  * Trade window, used when transferring items between {@link Container}s
@@ -13,6 +17,8 @@ import bloodandmithril.ui.components.ContextMenu;
  * @author Matt
  */
 public class TradeWindow extends Window {
+
+	Panel panel = new ScrollableListingPanel(this, Color.MAGENTA, 10, 10, 100, 100, Color.MAGENTA);
 
 	/**
 	 * Constructor
@@ -27,6 +33,7 @@ public class TradeWindow extends Window {
 		// 2 Panes, either top/bottom split, or left/right split, corresponding to the container being transferred from/to
 		// Arrows indicating moving items between containers
 		// Currency will be treated as an Item, not separately.
+		panel.render();
 	}
 
 
