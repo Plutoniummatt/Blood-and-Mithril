4702c2e8d9a57825ec2e1865c4ed6741be451738
plutomattp <plutomattp@66836f89-9df8-47b4-b0e3-7a98460e9a9e>
2014-02-28 17:47:38 +0000

    Move DummyComponent into its own class, code comments

---
+++ trunk/source/src/bloodandmithril/generation/component/DummyComponent.java
@@ -0,0 +1,55 @@
+package bloodandmithril.generation.component;
+
+import bloodandmithril.world.topography.tile.Tile;
+
+/**
+ * Dummy {@link Component} used as a placeholder when a component can not be
+ * generated.
+ * 
+ * @author Matt
+ */
+public class DummyComponent extends Component {
+	private static final long serialVersionUID = -695818207182117732L;
+
+	/** Singleton instance */
+	private static DummyComponent instance = new DummyComponent();
+
+	/**
+	 * @return the singleton DummyComponent instance
+	 */
+	public static DummyComponent getInstance() {
+		return instance;
+	}
+
+	
+	@Override
+	public Tile getForegroundTile(int worldTileX, int worldTileY) {
+		throw new UnsupportedOperationException("Attempting to get tile from a DummyComponent, something has gone horribly wrong!");
+	}
+
+	
+	@Override
+	public Tile getBackgroundTile(int worldTileX, int worldTileY) {
+		throw new UnsupportedOperationException("Attempting to get tile from a DummyComponent, something has gone horribly wrong!");
+	}
+
+	
+	@Override
+	protected void generateInterfaces() {
+		// Do nothing
+	}
+
+	
+	@Override
+	protected <T extends Component> Component internalStem(Class<T> with, ComponentCreationCustomization<T> custom) {
+		return getInstance();
+	}
+
+	
+	/**
+	 * Constructor - Do not use.
+	 */
+	private DummyComponent() {
+		super(null, -1);
+	}
+}
