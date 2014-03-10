3be505f13f16f1e344d38ca1e5e606530d388bc0
plutomattp <plutomattp@66836f89-9df8-47b4-b0e3-7a98460e9a9e>
2014-03-10 16:50:42 +0000

    Might need this...

---
+++ source/src/bloodandmithril/world/World.java
@@ -0,0 +1,67 @@
+package bloodandmithril.world;
+
+import java.io.Serializable;
+import java.util.Set;
+
+import com.google.common.collect.Sets;
+
+import bloodandmithril.character.Individual;
+import bloodandmithril.persistence.ParameterPersistenceService;
+import bloodandmithril.prop.Prop;
+import bloodandmithril.world.Domain.Light;
+import bloodandmithril.world.topography.Topography;
+
+/**
+ * A World, holds info about the {@link Topography}, {@link Prop}s, {@link Individual}s and any related entities.
+ *
+ * @author Matt
+ */
+public class World implements Serializable {
+	
+	/** Unique identifier of this {@link World} */
+	private final int worldId;
+	
+	/** Gravity on this world */
+	private float gravity;
+
+	/** Individuals that are currently in this {@link World} */
+	private Set<Integer> individuals 	= Sets.newHashSet();
+	
+	/** {@link Prop}s that are on this {@link World} */
+	private Set<Integer> props 			= Sets.newHashSet();
+	
+	/** {@link Light}s that are present on this {@link World} */
+	private Set<Integer> lights 		= Sets.newHashSet();
+	
+	/**
+	 * Constructor
+	 */
+	public World(float gravity) {
+		this.worldId = ParameterPersistenceService.getParameters().getNextWorldKey();
+		this.setGravity(gravity);
+		Domain.addTopography(worldId, new Topography(worldId));
+	}
+
+	
+	/**
+	 * @return the {@link #topography}
+	 */
+	public Topography getTopography() {
+		return Domain.getTopography(worldId);
+	}
+
+
+	public float getGravity() {
+		return gravity;
+	}
+
+
+	public void setGravity(float gravity) {
+		this.gravity = gravity;
+	}
+
+
+	public int getWorldId() {
+		return worldId;
+	}
+}
