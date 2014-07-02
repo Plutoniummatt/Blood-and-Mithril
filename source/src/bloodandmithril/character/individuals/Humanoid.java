a603410958fa959fcf01dc6bd439d97eb1995b16
plutomattp <plutomattp@66836f89-9df8-47b4-b0e3-7a98460e9a9e>
2014-07-02 17:03:49 +0000

    Stuff and things....related to combat and equipment rendering

--- source/src/bloodandmithril/character/individuals/Humanoid.java
+++ source/src/bloodandmithril/character/individuals/Humanoid.java
@@ -5,7 +5,10 @@ import static com.google.common.collect.Maps.newHashMap;
 import java.util.Map;
 
 import bloodandmithril.util.ParameterizedTask;
+import bloodandmithril.util.SpacialConfiguration;
 import bloodandmithril.util.datastructure.Box;
+
+import com.badlogic.gdx.math.Vector2;
 
 /**
  * Uses standard humanoid animations
@@ -52,5 +55,30 @@ public abstract class Humanoid extends GroundTravellingIndividual {
 	@Override
 	protected Map<Action, Map<Integer, ParameterizedTask<Individual>>> getActionFrames() {
 		return actionFrames;
+	}
+
+
+	@Override
+	protected SpacialConfiguration getOneHandedWeaponSpatialConfigration() {
+		int frameIndex = getCurrentAnimation().get(0).getKeyFrameIndex(getAnimationTimer());
+
+		switch(getCurrentAction()) {
+			case STAND_LEFT:
+				return new SpacialConfiguration(new Vector2(0, 40f), 0f, false);
+			case STAND_RIGHT:
+				return new SpacialConfiguration(new Vector2(0, 40f), 0f, true);
+			case WALK_LEFT:
+			case WALK_RIGHT:
+			case RUN_LEFT:
+			case RUN_RIGHT:
+			case ATTACK_LEFT_ONE_HANDED_WEAPON:
+			case ATTACK_LEFT_ONE_HANDED_WEAPON_STAB:
+			case ATTACK_RIGHT_ONE_HANDED_WEAPON:
+			case ATTACK_RIGHT_ONE_HANDED_WEAPON_STAB:
+				return new SpacialConfiguration(new Vector2(0, 40f), 0f, false);
+
+			default:
+				throw new RuntimeException("Unexpected action: " + getCurrentAction());
+		}
 	}
 }
