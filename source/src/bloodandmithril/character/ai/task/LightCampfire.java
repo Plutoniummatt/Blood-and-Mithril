2f5c5bf5ebd1931bbb3e6b3cbdc35d1841b3f05d
plutomattp <plutomattp@66836f89-9df8-47b4-b0e3-7a98460e9a9e>
2014-11-11 16:49:10 +0000

    Campfire lighting logic

---
+++ source/src/bloodandmithril/character/ai/task/LightCampfire.java
@@ -0,0 +1,86 @@
+package bloodandmithril.character.ai.task;
+
+import bloodandmithril.character.ai.AITask;
+import bloodandmithril.character.ai.pathfinding.Path.WayPoint;
+import bloodandmithril.character.ai.pathfinding.PathFinder;
+import bloodandmithril.character.individuals.Individual;
+import bloodandmithril.character.individuals.IndividualIdentifier;
+import bloodandmithril.prop.construction.craftingstation.Campfire;
+import bloodandmithril.world.Domain;
+import bloodandmithril.world.topography.Topography;
+
+/**
+ * Task that instructs the host to light a {@link Campfire}
+ *
+ * @author Matt
+ */
+public class LightCampfire extends CompositeAITask {
+	private static final long serialVersionUID = -2379179198784328909L;
+
+	private Campfire campfire;
+
+	/**
+	 * Constructor
+	 */
+	public LightCampfire(Individual host, Campfire campfire) {
+		super(
+			host.getId(),
+			"Mining",
+			new GoToLocation(
+				host,
+				new WayPoint(PathFinder.getGroundAboveOrBelowClosestEmptyOrPlatformSpace(campfire.position, 10, Domain.getWorld(host.getWorldId())), 3 * Topography.TILE_SIZE),
+				false,
+				50f,
+				true
+			)
+		);
+
+		this.campfire = campfire;
+		appendTask(new LightFire(hostId));
+	}
+
+
+	/**
+	 * Actual lighting of the fire
+	 *
+	 * @author Matt
+	 */
+	public class LightFire extends AITask {
+		private static final long serialVersionUID = -5213896264414790155L;
+
+
+		public LightFire(IndividualIdentifier hostId) {
+			super(hostId);
+		}
+
+
+		@Override
+		public String getDescription() {
+			return null;
+		}
+
+
+		@Override
+		public boolean isComplete() {
+			return false;
+		}
+
+
+		@Override
+		public boolean uponCompletion() {
+			return false;
+		}
+
+
+		@Override
+		public void execute(float delta) {
+			Individual host = Domain.getIndividual(hostId.getId());
+
+			if (host.getInteractionBox().isWithinBox(campfire.position)) {
+				if (Domain.hasProp(campfire.id)) {
+					campfire.setLit(true);
+				}
+			}
+		}
+	}
+}
