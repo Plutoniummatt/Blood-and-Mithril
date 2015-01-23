0395e8705073e90be9c3662a6044fe28b19bb655
plutomattp <plutomattp@66836f89-9df8-47b4-b0e3-7a98460e9a9e>
2015-01-23 22:55:10 +0000

    Remove unused/bad classes

--- trunk/source/src/bloodandmithril/generation/superstructure/Underground.java
+++ trunk/source/src/bloodandmithril/generation/superstructure/Underground.java
@@ -1,14 +1,11 @@
 package bloodandmithril.generation.superstructure;
 
 import bloodandmithril.core.Copyright;
-import bloodandmithril.generation.component.components.Cavern;
 import bloodandmithril.generation.patterns.GlobalLayers;
 import bloodandmithril.generation.tools.RectangularSpaceCalculator;
 import bloodandmithril.util.datastructure.Boundaries;
 import bloodandmithril.world.Domain;
-import bloodandmithril.world.topography.Topography;
 import bloodandmithril.world.topography.tile.Tile;
-import bloodandmithril.world.topography.tile.tiles.stone.CrystalTile;
 
 /**
  * A region of superstructre where cavity patterns are used
@@ -43,20 +40,6 @@ public class Underground extends SuperStructure {
 
 	@Override
 	protected void internalGenerate(boolean generatingToRight) {
-		int centerX = (getBoundaries().left + (getBoundaries().right - getBoundaries().left) / 2) * Topography.CHUNK_SIZE;
-		int centerY = (getBoundaries().bottom + (getBoundaries().top - getBoundaries().bottom) / 2) * Topography.CHUNK_SIZE;
-		getComponents().add(new Cavern(
-			new Boundaries(centerY + 150, centerY - 150, centerX - 200, centerX + 200),
-			getStructureKey(),
-			15,
-			new Boundaries(centerY + 100, centerY - 100, centerX - 150, centerX + 150),
-			30,
-			10,
-			40,
-			CrystalTile.class
-				
-		));
-		
 	}
 
 
