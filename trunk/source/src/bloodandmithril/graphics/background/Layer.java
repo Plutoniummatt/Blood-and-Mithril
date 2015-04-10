9cfb0d630548c4c1e28f582e315eb725710386b2
plutomattp <plutomattp@66836f89-9df8-47b4-b0e3-7a98460e9a9e>
2015-04-10 23:35:27 +0000

    Implement reflections, first pass

--- trunk/source/src/bloodandmithril/graphics/background/Layer.java
+++ trunk/source/src/bloodandmithril/graphics/background/Layer.java
@@ -1,6 +1,7 @@
 package bloodandmithril.graphics.background;
 
 import static bloodandmithril.core.BloodAndMithrilClient.WIDTH;
+import static bloodandmithril.core.BloodAndMithrilClient.cam;
 import static bloodandmithril.core.BloodAndMithrilClient.spriteBatch;
 import static bloodandmithril.graphics.background.BackgroundImages.textures;
 
@@ -20,75 +21,80 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 @Copyright("Matthew Peck 2015")
 public abstract class Layer implements Serializable {
 	private static final long serialVersionUID = 5749079115895910672L;
-	
+
 	/**
 	 * Maps the position of the x-coordinate of the image, to the image ID
 	 */
 	private final TreeMap<Integer, Integer> images;
-	
+
 	/**
 	 * Constructor
 	 */
 	protected Layer(TreeMap<Integer, Integer> images) {
 		this.images = images;
 	}
-	
-	
+
+
+	public static int getScreenHorizonY() {
+		return 400 - Math.round((int) cam.position.y * (1f - 0.95f));
+	}
+
+
 	/**
 	 * Renders this layer
 	 */
 	public void render(int camX, int camY) {
-		int startPositionX = Math.round(((camX - WIDTH / 2) * (1f - getDistanceX())));
-		int startPositionY = Math.round((camY * (1f - getDistanceY())));
+		int startPositionX = Math.round((camX - WIDTH / 2) * (1f - getDistanceX()));
+		int startPositionY = Math.round(camY * (1f - getDistanceY()));
 		int currentPosition = 0;
-		
+
 		Entry<Integer, Integer> floorEntry = images.floorEntry(startPositionX);
 		if (floorEntry == null) {
 			return;
 		}
 		float startingRenderingPosition = (camX - WIDTH/2) * (1f - getDistanceX()) - floorEntry.getKey();
-		
+
 		boolean rendering = true;
 		while(rendering) {
 			if (currentPosition - startingRenderingPosition > WIDTH) {
 				rendering = false;
 			}
-			
+
 			TextureRegion toDraw = textures.get(images.floorEntry(startPositionX + currentPosition).getValue());
-			
+
 			if (toDraw == null) {
 				return;
 			}
-			
+
 			spriteBatch.draw(
 				toDraw,
 				currentPosition - startingRenderingPosition,
 				getOffsetY() - startPositionY
 			);
-			
+
 			currentPosition += toDraw.getRegionWidth();
 		}
 	}
-	
-	
+
+
 	/**
 	 * Applies pre-render logic like shaders
 	 */
 	public abstract void preRender();
-	
-	
+
+
 	/**
 	 * @return the perceived distance of this layer from foreground, 1f is infinity
 	 */
 	public abstract float getDistanceX();
-	
-	
+
+
 	/**
 	 * @return the perceived distance of this layer from foreground, 1f is infinity
 	 */
 	public abstract float getDistanceY();
-	
-	
+
+
 	/**
 	 * @return the vertical render offset
 	 */
