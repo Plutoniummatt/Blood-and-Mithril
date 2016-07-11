5fa767d51e1daab937366e3ae4a35729ba2b85ae
Plutoniummatt <plutoniummatt@hotmail.com>
2016-07-11 20:54:55 +0100

    Revert "Refactor"
    This reverts commit 4a393daeaecfa1fd1688dfd80069b315503b275a.

---
+++ application/src/trunkapp/Main.java
@@ -0,0 +1,39 @@
+package trunkapp;
+
+import com.badlogic.gdx.Files.FileType;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import bloodandmithril.core.BloodAndMithrilClient;
+import bloodandmithril.core.Copyright;
+import bloodandmithril.persistence.ConfigPersistenceService;
+
+/**
+ * The entry point of the game
+ *
+ * @author Matt
+ */
+@Copyright("Matthew Peck 2014")
+public class Main {
+	public static BloodAndMithrilClient client;
+
+	public static void main(String[] args) {
+
+	  //Configurations
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "Blood and Mithril";
+		cfg.useGL30 = false;
+		cfg.samples = 4;
+		cfg.width = ConfigPersistenceService.getConfig().getResX();
+		cfg.height = ConfigPersistenceService.getConfig().getResY();
+		cfg.fullscreen = ConfigPersistenceService.getConfig().isFullScreen();
+		cfg.resizable = true;
+		cfg.addIcon("data/image/smallIcon.png", FileType.Internal);
+		cfg.addIcon("data/image/icon.png", FileType.Internal);
+
+		BloodAndMithrilClient.devMode = true;
+		client = new BloodAndMithrilClient();
+
+		new LwjglApplication(client, cfg);
+	}
+}
