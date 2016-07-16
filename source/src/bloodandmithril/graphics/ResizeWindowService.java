b6d0a2cdcc890175f14d6e7ac6929c60ca823dc7
Plutoniummatt <plutoniummatt@hotmail.com>
2016-07-16 04:00:05 -0700

    Add missing class

---
+++ source/src/bloodandmithril/graphics/ResizeWindowService.java
@@ -0,0 +1,38 @@
+package bloodandmithril.graphics;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+import bloodandmithril.core.Copyright;
+import bloodandmithril.ui.UserInterface;
+
+/**
+ * Handles resizing of the game window
+ *
+ * @author Matt
+ */
+@Singleton
+@Copyright("Matthew Peck 2016")
+public class ResizeWindowService {
+	
+	@Inject private Graphics graphics;
+	@Inject private UserInterface userInterface;
+
+	/**
+	 * Processes graphics side of resizing the window
+	 */
+	public void resize(final int newWidth, final int newHeight) {
+		final int oldWidth = graphics.getWidth();
+		final int oldHeight = graphics.getHeight();
+		
+		graphics.resize(newWidth, newHeight);
+		
+		UserInterface.resetWindowPositions(oldWidth, oldHeight);
+		
+		userInterface.getUICamera().setToOrtho(false, graphics.getWidth(), graphics.getHeight());
+		userInterface.getUITrackingCamera().setToOrtho(false, graphics.getWidth(), graphics.getHeight());
+
+		UserInterface.shapeRenderer.setProjectionMatrix(userInterface.getUICamera().projection);
+		UserInterface.shapeRenderer.setTransformMatrix(userInterface.getUICamera().view);
+	}
+}
